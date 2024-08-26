ac7c8f58b9c046fc633acb099079619cc5fdf132
xdenlz <106308342+xidenlz@users.noreply.github.com>
2024-08-26 06:30:50 -0700

    Update CFG-Lib.cmake

--- Build/cmake/CFG-Lib.cmake
+++ Build/cmake/CFG-Lib.cmake
@@ -1,12 +1,10 @@
 include(FetchContent)
 
-message(STATUS "Setting up nlohmann::json")
+message(STATUS "CFG-Lib")
 FetchContent_Declare(
     json
-    GIT_REPOSITORY "https://github.com/nlohmann/json/"
-    GIT_TAG v3.11.2
+    GIT_REPOSITORY "https://github.com/xidenlz/CFG-Library"
+    GIT_TAG CFG-Lib
     GIT_SHALLOW TRUE
 )
-option(JSON_MultipleHeaders "Disable nlohmann JSON multi header default." OFF)
-
 FetchContent_MakeAvailable(json)
